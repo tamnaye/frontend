18216c9aa5d3180ca26259e516f6320ce59245bc
hyunjeong <cchloe0927@gmail.com>
2022-08-23 15:32:09 +0900

    키워드 - 예약페이지 데이터 받아오기, 단위 #issue1
    변경사항 : 예약페이지 룸 이름, 룸 Id, 이미지는 로컬자체에 이미지 저장해서 불러오기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.module.css';
 import Login from './component/login/Login';
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from 'react-router-dom';
 import EmptyPage from './component/EmptyPage';
 import Header from './component/header/Header';
 import MainTemplate from './component/main/MainTemplate';
-import ReservationState from './component/reservation/ReservationState'
-import BookPage from './component/bookpage/BookPage'
-import MyPage from './component/mypage/MyPage'
+import ReservationState from './component/reservation/ReservationState';
+import BookPage from './component/bookpage/BookPage';
+import MyPage from './component/mypage/MyPage';
 
 function App() {
   let location = useLocation();
@@ -17,12 +17,11 @@ function App() {
         <Route path='login' element={<Login />} />
         <Route path='/' element={<MainTemplate />} />
         <Route path='main/:id' element={<MainTemplate />} />
-        <Route path="/state" element={<ReservationState />} />
-        <Route path='/booking/:spaceName' element={<BookPage/>} />
+        <Route path='/state' element={<ReservationState />} />
+        <Route path='/booking/:id' element={<BookPage />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='*' element={<EmptyPage />} />
       </Routes>
-
     </div>
   );
 }
