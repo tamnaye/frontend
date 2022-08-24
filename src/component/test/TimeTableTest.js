43cc8d857888cb6b26fbeae8968f264e0892efa3
hyunjeong <cchloe0927@gmail.com>
2022-08-24 10:14:25 +0900

    커밋

--- src/component/test/TimeTableTest.js
+++ src/component/test/TimeTableTest.js
@@ -1,7 +1,7 @@
-import { Button } from "react-bootstrap";
-import { useState } from "react";
-import dummy from "../db/booking_data.json";
-import "./Timetable_test.module.css";
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import dummy from '../../db/booking_data.json';
+import './Timetable_test.module.css';
 
 function TimeTableTest() {
   //length = 12
@@ -10,18 +10,18 @@ function TimeTableTest() {
   booking_data.map((booking) => timeArray.push(booking.time_start));
   booking_data.map((booking) => timeArray.push(booking.time_end));
   const baseTables = [
-    { id: 1, time: "09:00", current: false },
-    { id: 2, time: "10:00", current: false },
-    { id: 3, time: "11:00", current: false },
-    { id: 4, time: "12:00", current: false },
-    { id: 5, time: "13:00", current: false },
-    { id: 6, time: "14:00", current: false },
-    { id: 7, time: "15:00", current: false },
-    { id: 8, time: "16:00", current: false },
-    { id: 9, time: "17:00", current: false },
-    { id: 10, time: "18:00", current: false },
-    { id: 11, time: "19:00", current: false },
-    { id: 12, time: "20:00", current: false },
+    { id: 1, time: '09:00', current: false },
+    { id: 2, time: '10:00', current: false },
+    { id: 3, time: '11:00', current: false },
+    { id: 4, time: '12:00', current: false },
+    { id: 5, time: '13:00', current: false },
+    { id: 6, time: '14:00', current: false },
+    { id: 7, time: '15:00', current: false },
+    { id: 8, time: '16:00', current: false },
+    { id: 9, time: '17:00', current: false },
+    { id: 10, time: '18:00', current: false },
+    { id: 11, time: '19:00', current: false },
+    { id: 12, time: '20:00', current: false },
   ];
   let currentTimes = baseTables.map((times) =>
     timeArray.includes(times.time) ? { ...times, current: true } : times
@@ -45,9 +45,9 @@ function TimeTableTest() {
         <span key={time.id}>
           {category(time.id)}
           <Button
-            variant="success"
+            variant='success'
             disabled={time.current}
-            style={{ margin: "10px" }}
+            style={{ margin: '10px' }}
           >
             {time.time}
           </Button>
