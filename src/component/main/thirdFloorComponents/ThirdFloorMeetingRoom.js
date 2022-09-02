450f054c1e7d33757f8eadbf96ec9532ef25ade9
SooBin An <jejupalette@naver.com>
2022-09-02 14:26:42 +0900

    fix : 시간 관련 버튼 수정사항 변경

--- src/component/main/thirdFloorComponents/ThirdFloorMeetingRoom.js
+++ src/component/main/thirdFloorComponents/ThirdFloorMeetingRoom.js
@@ -1,58 +1,70 @@
-import { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import styles from './ThirdFloorMeetingRoom.module.css';
-import useUrl from '../../../hooks/useUrl';
-import useTimeAlert from '../../../hooks/useTimeAlert';
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './ThirdFloorMeetingRoom.module.css'
+import useUrl from '../../../hooks/useUrl'
+import useTimeAlert from '../../../hooks/useTimeAlert'
 
 const ThirdFloorMeetingRoom = () => {
-  const { id } = useParams();
-  const myUrl = useUrl();
-  const [ablebtn, BookingConfirm] = useTimeAlert();
+  const myUrl = useUrl()
+  const [ablebtn, BookingConfirm] = useTimeAlert()
 
   //3층 미팅룸 API 사용 정보 불러오기
-  const [bookingData, setBookingData] = useState([]);
-  const [roomData, setRoomData] = useState([]);
+  const [bookingData, setBookingData] = useState([])
+  const [roomData, setRoomData] = useState([])
 
+  const url = `http://${myUrl}/api/booking/main?floor=3`
   useEffect(() => {
-    fetch(`http://${myUrl}/api/booking/main?floor=3`, {
+    fetch(url, {
       method: 'GET',
     })
       .then((res) => res.json())
       .then((data) => {
-        setBookingData(data.BookingData);
-        setRoomData(data.RoomData);
-      });
-  }, [`htttp://${myUrl}/api/booking/main?floor=3`]);
+        setBookingData(data.BookingData)
+        setRoomData(data.RoomData)
+      })
+  }, [url, myUrl])
 
   const ThirdMeetingRoominfo = roomData.filter(
     (rooms) => rooms.roomType === 'meeting'
-  );
+  )
 
-  // roomFull 함수
+  // roomFull 함수 설정
+  // 현재 시간을 통해 남은 예약 가능 시간 확인
+  const Now = new Date()
+  const NowHour = Now.getHours()
+  const OverHour = NowHour + 1
+  const RemainTime = 21 - OverHour
+  // console.log(RemainTime)
 
   const notroomFull = (roomid) => {
-    const roomState = bookingData.filter((room) => room.roomId === roomid);
-
+    // 09:00 과 같은 형태 9로 수정해주는 함수
     const TimeToString = (time) => {
-      let newTime;
+      let newTime
       if (time === '09:00') {
-        newTime = time.substr(1, 1);
+        newTime = time.substr(1, 1)
       } else {
-        newTime = time.substr(0, 2);
+        newTime = time.substr(0, 2)
       }
-      return newTime;
-    };
-
+      return newTime
+    }
+    // room아이디와 현재 시간으로 거른 예약 현황
+    const roomState = bookingData.filter(
+      (room) =>
+        room.roomId === roomid &&
+        Number(TimeToString(room.startTime)) >= Number(OverHour)
+    )
+    // 거른 예약 현황을 대상으로 시간 추출 리스트
     const roomBookingState = roomState.map(
       (room) =>
         TimeToString(room.endTime) - Number(TimeToString(room.startTime))
-    );
+    )
+    // 추출한 시간 합치기
     const sum = roomBookingState.reduce(function add(sum, currValue) {
-      return sum + currValue;
-    }, 0);
-
-    return sum !== 12;
-  };
+      return sum + currValue
+    }, 0)
+    // 합과 남은 시간 불린으로 결과값 제출
+    return sum < RemainTime
+  }
 
   return (
     <div className={styles.MeetingRoomContainer}>
@@ -74,7 +86,7 @@ const ThirdFloorMeetingRoom = () => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ThirdFloorMeetingRoom;
+export default ThirdFloorMeetingRoom
