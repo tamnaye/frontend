651aa65ba34ca4c8c4f6cd81c69a66c3570a32ea
hyunjeong <cchloe0927@gmail.com>
2022-09-03 02:56:12 +0900

    피드백 제출 버튼

--- src/component/Feedback.js
+++ src/component/Feedback.js
@@ -1,18 +1,28 @@
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 import { Container } from 'react-bootstrap';
 
 const Feedback = () => {
   return (
     <Container style={{ marginTop: '60px' }}>
-      <Form.Label htmlFor='basic-url'>
-        소중한 피드백 감사합니다🧡 아래에 작성해 주세요!
-      </Form.Label>
-
-      <InputGroup style={{ marginTop: '10px' }}>
-        <InputGroup.Text>피드백</InputGroup.Text>
-        <Form.Control as='textarea' aria-label='With textarea' />
-      </InputGroup>
+      <Form>
+        <p>소중한 피드백 감사합니다🧡 아래에 작성해 주세요!</p>
+        <InputGroup style={{ marginTop: '10px' }}>
+          <InputGroup.Text>피드백</InputGroup.Text>
+          <Form.Control as='textarea' aria-label='With textarea' />
+        </InputGroup>
+        <Button
+          style={{
+            marginTop: '20px',
+            float: 'right',
+          }}
+          variant='primary'
+          type='submit'
+        >
+          Submit
+        </Button>
+      </Form>
     </Container>
   );
 };
