811f6d3e37817a93aea874ae30235c978c07f395
hyunjeong <cchloe0927@gmail.com>
2022-09-03 16:08:18 +0900

    피드백 페이지 완료

--- src/component/Feedback.js
+++ src/component/Feedback.js
@@ -2,27 +2,94 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+//hooks
+import { useEffect, useRef, useState } from 'react';
+import useUrl from '../hooks/useUrl';
 
 const Feedback = () => {
+  const myUrl = useUrl();
+  const userid = window.localStorage.getItem('userid');
+  const feedbackRef = useRef(null);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    //console.log(feedbackRef.current.value);
+
+    //feadback POST
+    fetch(`http://${myUrl}/api/feedback`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: feedbackRef.current.value,
+        userId: userid,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        if (data.message) {
+          alert('피드백 주셔서 감사합니다😍');
+        }
+      });
+  };
+
+  //feadback GET
+  const [feedbackDatas, setFeedbackDatas] = useState([]);
+  const url = `http://${myUrl}/api/feedback`;
+  useEffect(() => {
+    fetch(url, { method: 'GET' })
+      .then((res) => res.json())
+      .then((data) => {
+        setFeedbackDatas(data.FeedbackData);
+      });
+  }, [url]);
+  console.log(feedbackDatas);
+
   return (
     <Container style={{ marginTop: '60px' }}>
-      <Form>
-        <p>소중한 피드백 감사합니다🧡 아래에 작성해 주세요!</p>
-        <InputGroup style={{ marginTop: '10px' }}>
-          <InputGroup.Text>피드백</InputGroup.Text>
-          <Form.Control as='textarea' aria-label='With textarea' />
-        </InputGroup>
-        <Button
-          style={{
-            marginTop: '20px',
-            float: 'right',
-          }}
-          variant='primary'
-          type='submit'
-        >
-          Submit
-        </Button>
-      </Form>
+      <div>
+        <Form onSubmit={onSubmit}>
+          <p style={{ fontWeight: 'bold' }}>
+            아래에 작성해 주세요! (피드백 기간에만 오픈할 예정입니다.)
+          </p>
+          <InputGroup style={{ marginTop: '10px' }}>
+            <InputGroup.Text>피드백</InputGroup.Text>
+            <Form.Control
+              as='textarea'
+              aria-label='With textarea'
+              ref={feedbackRef}
+            />
+          </InputGroup>
+          <Button
+            style={{
+              display: 'block',
+              margin: '30px auto',
+              fontSize: '15px',
+            }}
+            variant='primary'
+            type='submit'
+          >
+            Submit
+          </Button>
+        </Form>
+      </div>
+      {userid === '22106060' ||
+      userid === '22106040' ||
+      userid === '22106045' ||
+      userid === '22106069' ? (
+        <div>
+          <p style={{ marginTop: '50px', fontWeight: 'bold' }}>피드백 리스트</p>
+          <hr />
+          {feedbackDatas.map((item, index) => (
+            <Card key={index} style={{ marginTop: '20px' }}>
+              <Card.Body>{item}</Card.Body>
+            </Card>
+          ))}
+        </div>
+      ) : null}
     </Container>
   );
 };
