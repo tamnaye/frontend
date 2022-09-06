e18f0a93d33a50bf7473c059ef0f17187a344181
SooBin An <jejupalette@naver.com>
2022-09-06 18:05:41 +0900

    pull을 위한 push

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,7 +1,5 @@
 export default function useUrl() {
-
-  const url = '192.168.5.107:8080';
-  // const url = '144.24.91.218:8080';
-  return url;
-
+  // const url = '192.168.5.107:8080';
+  const url = '144.24.91.218:8080'
+  return url
 }
