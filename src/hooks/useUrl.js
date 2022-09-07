65c374c6555887354339c27dc99c7b0a0a8070b8
hyunjeong <cchloe0927@gmail.com>
2022-09-07 16:16:53 +0900

    충돌해결

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,9 +1,5 @@
 export default function useUrl() {
-<<<<<<< HEAD
-  // const url = '192.168.5.107:8080';
-=======
   // const url = '192.168.5.100:8080';
->>>>>>> f77d04278c6af86a79911806a9a09afbf004114f
   const url = '144.24.91.218:8080';
   return url;
 }
