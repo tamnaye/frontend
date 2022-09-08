2bcabec12d9618c4f062a22a7b671186c84c9ce5
SooBin An <jejupalette@naver.com>
2022-09-08 09:58:06 +0900

    design : 3층 스튜디오 예약현황 팀원 삭제

--- src/component/reservation/ThirdFloorStateComponents/ThirdFloorMeetingRoomState.js
+++ src/component/reservation/ThirdFloorStateComponents/ThirdFloorMeetingRoomState.js
@@ -4,6 +4,7 @@ import styles from './ThirdFloorMeetingRoomState.module.css'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Popover from 'react-bootstrap/Popover'
 import Poplay from '../Poplay'
+import PoplayNabox from '../PoplayNabox'
 import { Link } from 'react-router-dom'
 import {
   EmojiSmileFill,
@@ -11,192 +12,248 @@ import {
   Calendar2CheckFill,
 } from 'react-bootstrap-icons'
 import useUrl from '../../../hooks/useUrl'
-import { useNavigate } from 'react-router-dom'
 
-const ThirdFloorMeetingRoomState = () => {
-  // API 3층 회의실 가져오기
-  const [bookingData, setBookingData] = useState([])
-  const [roomData, setRoomData] = useState([])
-  const navigate = useNavigate()
+// import { useNavigate } from 'react-router-dom'
 
-  const myUrl = useUrl()
+const ThirdFloorMeetingRoomState = () =>
+  // bookingData,
+  // roomData,
+  // ThirdMeetingStudioinfo,
+  {
+    // API 3층 회의실 가져오기
+    const [bookingData, setBookingData] = useState([])
+    const [roomData, setRoomData] = useState([])
+    // const navigate = useNavigate()
 
-  const url = `http://${myUrl}/api/booking/details-booking?floor=3`
-  useEffect(() => {
-    fetch(url, {
-      method: 'GET',
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setBookingData(data.BookingData)
-        setRoomData(data.RoomData)
+    const myUrl = useUrl()
+    const url = `http://${myUrl}/api/booking/details-booking?floor=3`
+    useEffect(() => {
+      fetch(url, {
+        method: 'GET',
       })
-  }, [url, myUrl])
+        .then((res) => res.json())
+        .then((data) => {
+          setBookingData(data.BookingData)
+          setRoomData(data.RoomData)
+        })
+    }, [url, myUrl])
 
-  const ThirdMeetingStudioinfo = roomData.filter(
-    (rooms) => rooms.roomType === 'meeting' || rooms.roomType === 'studio'
-  )
-
-  // 타임 리스트 돌리기
-  let timeList = []
-  for (let i = 9; i <= 20; i++) {
-    timeList.push(i + '시')
-  }
-
-  // 09:00 형태 9로 숫자만 뽑아주는 함수
-  const TimeToString = (time) => {
-    let newTime
-    if (time === '09:00') {
-      newTime = time.substr(1, 1)
-    } else {
-      newTime = time.substr(0, 2)
-    }
-    return newTime
-  }
-
-  // 9시 형태 9로 숫자만 뽑아주는 함수
-  const onlyTime = (time) => {
-    let newTime
-    if (time === '9시') {
-      newTime = time.substr(0, 1)
-    } else {
-      newTime = time.substr(0, 2)
-    }
-    return newTime
-  }
-
-  // 시간당 룸의 예약 데이터 불러오는 함수
-  const TimeAndRoomFilter = (Time, Room) => {
-    let timedata = bookingData.filter(
-      (room) =>
-        room.roomId === Room && TimeToString(room.startTime) === onlyTime(Time)
+    const ThirdMeetingStudioinfo = roomData.filter(
+      (rooms) => rooms.roomType === 'meeting' || rooms.roomType === 'studio'
     )
-    return timedata
-  }
 
-  // 시간당 룸의 예약이 있는지 없는지 함수
-  const IsThisTimeRoombooked = (Time, Room) => {
-    const IsTrue = TimeAndRoomFilter(Time, Room).length !== 0
-    return IsTrue
-  }
+    // 타임 리스트 돌리기
+    let timeList = []
+    for (let i = 9; i <= 20; i++) {
+      timeList.push(i + '시')
+    }
 
-  // 예약 시간 함수
-  const bookingLength = (startTime, endTime) => {
-    let length = Number(TimeToString(endTime)) - Number(TimeToString(startTime))
-    return length
-  }
-  // console.log(navigate(`/booking/${301}`))
+    // 09:00 형태 9로 숫자만 뽑아주는 함수
+    const TimeToString = (time) => {
+      let newTime
+      if (time === '09:00') {
+        newTime = time.substr(1, 1)
+      } else {
+        newTime = time.substr(0, 2)
+      }
+      return newTime
+    }
 
-  return (
-    <div className={styles.tableBox}>
-      <table className="table table-bordered" id={styles.table}>
-        <thead className="table-light" id={styles.thead}>
-          <tr id={styles.theadTr}>
-            <th className="table-primary" id={styles.time}></th>
+    // 9시 형태 9로 숫자만 뽑아주는 함수
+    const onlyTime = (time) => {
+      let newTime
+      if (time === '9시') {
+        newTime = time.substr(0, 1)
+      } else {
+        newTime = time.substr(0, 2)
+      }
+      return newTime
+    }
 
-            {/* 룸 값 불러오기 */}
-            {ThirdMeetingStudioinfo.map((room) => (
-              <th key={room.roomId} className="table-primary" id={styles.text}>
-                <Link to={`/booking/${room.roomId}`} id={styles.roomTitle}>
-                  <ArrowRightCircleFill />
-                  {'\n'}
-                  {room.roomName}
-                </Link>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody id={styles.tbody}>
-          {/* 시간을 맵으로 돌려 전체 상태값 전달 */}
-          {timeList.map((time) => (
-            <tr key={time} id={styles.tbodyTr}>
-              <th className={styles.time}>{time}</th>
+    // 시간당 룸의 예약 데이터 불러오는 함수
+    const TimeAndRoomFilter = (Time, Room) => {
+      let timedata = bookingData.filter(
+        (room) =>
+          room.roomId === Room &&
+          TimeToString(room.startTime) === onlyTime(Time)
+      )
+      return timedata
+    }
 
-              {/* 룸을 맵으로 돌려 하나의 시간에 상태값 전달 */}
+    // 시간당 룸의 예약이 있는지 없는지 함수
+    const IsThisTimeRoombooked = (Time, Room) => {
+      const IsTrue = TimeAndRoomFilter(Time, Room).length !== 0
+      return IsTrue
+    }
+
+    // 예약 시간 함수
+    const bookingLength = (startTime, endTime) => {
+      let length =
+        Number(TimeToString(endTime)) - Number(TimeToString(startTime))
+      return length
+    }
+    // console.log(navigate(`/booking/${301}`))
+
+    return (
+      <div className={styles.tableBox}>
+        <table className="table table-bordered" id={styles.table}>
+          <thead className="table-light" id={styles.thead}>
+            <tr id={styles.theadTr}>
+              <th className="table-primary" id={styles.time}></th>
+
+              {/* 룸 값 불러오기 */}
               {ThirdMeetingStudioinfo.map((room) => (
                 <th
                   key={room.roomId}
-                  className={styles.roomstate}
-                  // onClick={() => navigate(`/booking/${room.roomId}`)}
+                  className="table-primary"
+                  id={styles.text}
                 >
-                  {IsThisTimeRoombooked(time, room.roomId) ? (
-                    <OverlayTrigger
-                      trigger={['hover', 'focus']}
-                      key={TimeAndRoomFilter(time, room.roomId)[0].bookingId}
-                      placement="top"
-                      overlay={
-                        <Popover id="popover-positioned-top">
-                          <Popover.Body>
-                            <Poplay
-                              userName={
-                                TimeAndRoomFilter(time, room.roomId)[0]
-                                  .applicant.userName
-                              }
-                              startTime={
-                                TimeAndRoomFilter(time, room.roomId)[0]
-                                  .startTime
-                              }
-                              endTime={
-                                TimeAndRoomFilter(time, room.roomId)[0].endTime
-                              }
-                              roomName={
-                                TimeAndRoomFilter(time, room.roomId)[0].roomName
-                              }
-                              participants={
-                                TimeAndRoomFilter(time, room.roomId)[0]
-                                  .participants
-                              }
-                            />
-                          </Popover.Body>
-                        </Popover>
-                      }
-                    >
-                      <button
-                        style={{
-                          height: `${
-                            bookingLength(
-                              TimeAndRoomFilter(time, room.roomId)[0].startTime,
-                              TimeAndRoomFilter(time, room.roomId)[0].endTime
-                            ) * 35.8
-                          }px`,
-                        }}
-                        className={styles.bookingTime}
-                        id={
-                          TimeAndRoomFilter(time, room.roomId)[0].official
-                            ? [styles.Manager]
-                            : null
-                        }
-                        variant="secondary"
-                      >
-                        {TimeAndRoomFilter(time, room.roomId)[0].official
-                          ? [
-                              <p key={0}>
-                                <Calendar2CheckFill />
-                                &nbsp; 공식 일정
-                              </p>,
-                            ]
-                          : [
-                              <p key={1}>
-                                <EmojiSmileFill />
-                                {'\n'}
-                                {
-                                  TimeAndRoomFilter(time, room.roomId)[0]
-                                    .applicant.userName
-                                }
-                                {/* 님 예약 */}
-                              </p>,
-                            ]}
-                      </button>
-                    </OverlayTrigger>
-                  ) : null}
+                  <Link to={`/booking/${room.roomId}`} id={styles.roomTitle}>
+                    <ArrowRightCircleFill />
+                    {'\n'}
+                    {room.roomName}
+                  </Link>
                 </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+          </thead>
+          <tbody id={styles.tbody}>
+            {/* 시간을 맵으로 돌려 전체 상태값 전달 */}
+            {timeList.map((time) => (
+              <tr key={time} id={styles.tbodyTr}>
+                <th className={styles.time}>{time}</th>
+
+                {/* 룸을 맵으로 돌려 하나의 시간에 상태값 전달 */}
+                {ThirdMeetingStudioinfo.map((room) => (
+                  <th
+                    key={room.roomId}
+                    className={styles.roomstate}
+                    // onClick={() => navigate(`/booking/${room.roomId}`)}
+                  >
+                    {IsThisTimeRoombooked(time, room.roomId) ? (
+                      <OverlayTrigger
+                        trigger={['hover', 'focus']}
+                        key={TimeAndRoomFilter(time, room.roomId)[0].bookingId}
+                        placement="top"
+                        overlay={
+                          <Popover id="popover-positioned-top">
+                            <Popover.Body>
+                              {room.roomId === 308 ? (
+                                <PoplayNabox
+                                  userName={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .applicant.userName
+                                  }
+                                  startTime={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .startTime
+                                  }
+                                  endTime={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .endTime
+                                  }
+                                  roomName={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .roomName
+                                  }
+                                />
+                              ) : (
+                                <Poplay
+                                  userName={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .applicant.userName
+                                  }
+                                  startTime={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .startTime
+                                  }
+                                  endTime={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .endTime
+                                  }
+                                  roomName={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .roomName
+                                  }
+                                  participants={
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .participants
+                                  }
+                                />
+                              )}
+                              {/* <Poplay
+                                userName={
+                                  TimeAndRoomFilter(time, room.roomId)[0]
+                                    .applicant.userName
+                                }
+                                startTime={
+                                  TimeAndRoomFilter(time, room.roomId)[0]
+                                    .startTime
+                                }
+                                endTime={
+                                  TimeAndRoomFilter(time, room.roomId)[0]
+                                    .endTime
+                                }
+                                roomName={
+                                  TimeAndRoomFilter(time, room.roomId)[0]
+                                    .roomName
+                                }
+                                participants={
+                                  TimeAndRoomFilter(time, room.roomId)[0]
+                                    .participants
+                                }
+                              /> */}
+                            </Popover.Body>
+                          </Popover>
+                        }
+                      >
+                        <button
+                          style={{
+                            height: `${
+                              bookingLength(
+                                TimeAndRoomFilter(time, room.roomId)[0]
+                                  .startTime,
+                                TimeAndRoomFilter(time, room.roomId)[0].endTime
+                              ) * 35.8
+                            }px`,
+                          }}
+                          className={styles.bookingTime}
+                          id={
+                            TimeAndRoomFilter(time, room.roomId)[0].official
+                              ? [styles.Manager]
+                              : null
+                          }
+                          variant="secondary"
+                        >
+                          {TimeAndRoomFilter(time, room.roomId)[0].official
+                            ? [
+                                <p key={0}>
+                                  <Calendar2CheckFill />
+                                  &nbsp; 공식 일정
+                                </p>,
+                              ]
+                            : [
+                                <p key={1}>
+                                  <EmojiSmileFill />
+                                  {'\n'}
+                                  {
+                                    TimeAndRoomFilter(time, room.roomId)[0]
+                                      .applicant.userName
+                                  }
+                                  {/* 님 예약 */}
+                                </p>,
+                              ]}
+                        </button>
+                      </OverlayTrigger>
+                    ) : null}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 
 export default ThirdFloorMeetingRoomState
