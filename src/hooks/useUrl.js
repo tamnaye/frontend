1bc4fcabecc4db5edf9a027025f1c446ca6f201d
hyunjeong <cchloe0927@gmail.com>
2022-09-14 15:26:36 +0900

    favicon update

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,4 +1,5 @@
 export default function useUrl() {
-  const url = process.env.REACT_APP_BACK_URL;
+  //const url = process.env.REACT_APP_BACK_URL;
+  const url = process.env.REACT_APP_BACK_Dev_URL;
   return url;
 }
