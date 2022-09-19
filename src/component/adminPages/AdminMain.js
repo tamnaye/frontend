4fbb821ad12635609b0e03de3abd4ca058c8d7d8
hyunjeong <cchloe0927@gmail.com>
2022-09-19 16:26:42 +0900

    adminMain컴포넌트 생성
    변경사항 : adminMain컴포넌트 생성

---
+++ src/component/adminPages/AdminMain.js
@@ -0,0 +1,11 @@
+import FileUpload from './fileUpload/FileUpload';
+
+const AdminMain = () => {
+  return (
+    <div>
+      <FileUpload />
+    </div>
+  );
+};
+
+export default AdminMain;
--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,5 +1,5 @@
 export default function useUrl() {
   // const url = process.env.REACT_APP_BACK_URL_TEST;
-  const url = "192.168.5.56:8080"
+  const url = process.env.REACT_APP_BACK_Dev_URL;
   return url;
 }
