ec37044464826d254b710ad61c3e4d319bb459dc
mugyeol <htpadkorik@naver.com>
2022-09-20 18:05:37 +0900

    admin_Room completed && admin_header 10%

--- src/component/adminPages/AdminMain.js
+++ src/component/adminPages/AdminMain.js
@@ -5,8 +5,18 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 //component
 import FileUpload from './fileUpload/FileUpload';
 import ClassesFloor from './classesFloor/ClassesFloor';
-
+import { useEffect } from 'react';
+import { fetchGet } from '../../hooks/fetchUrl';
+import useUrl from '../../hooks/useUrl';
+import { useNavigate } from 'react-router-dom';
 const AdminMain = () => {
+  
+  const navigate = useNavigate()
+  const url = `http://${useUrl()}/api/user/data`;
+  useEffect(()=>{
+    fetchGet(url,navigate).then((data)=>console.log("adminMain : ",data.userData))
+
+  })  
   return (
     <>
       {['lg'].map((expand) => (
@@ -26,8 +36,8 @@ const AdminMain = () => {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className='justify-content-end flex-grow-1 pe-3'>
-                  <Nav.Link href='/admin'>최신기수 업로드</Nav.Link>
-                  <Nav.Link href='#action2'>개별 인재관리</Nav.Link>
+                  <Nav.Link href='/admin/upload'>최신기수 업로드</Nav.Link>
+                  <Nav.Link href='/admin/individual'>개별 인재관리</Nav.Link>
                   <Nav.Link href='/admin/floor'>기수별 층수관리</Nav.Link>
                   <Nav.Link href='/admin/room'>회의실 데이터</Nav.Link>
                 </Nav>
