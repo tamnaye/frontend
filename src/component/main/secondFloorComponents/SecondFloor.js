2d423e47bd3962f89d7c5f1ffd38e08b32eb8058
SooBin An <jejupalette@naver.com>
2022-09-20 13:18:56 +0900

    refactor : 메인 전체 데이터 구조 MainTemplate에서 props로 받아오도록변경

--- src/component/main/secondFloorComponents/SecondFloor.js
+++ src/component/main/secondFloorComponents/SecondFloor.js
@@ -1,48 +1,24 @@
-import SecondFloorMap from './SecondFloorMap'
-import SecondFloorMeetingRoom from './SecondFloorMeetingRoom'
-import SecondFloorNaRoom from './SecondFloorNaRoom'
-import styles from './SecondFloor.module.css'
-import useUrl from '../../../hooks/useUrl'
-import useTimeAlert from '../../../hooks/useTimeAlert'
-import { useState, useEffect } from 'react'
-import { fetchGet } from '../../../hooks/fetchUrl'
-import { useLocation } from 'react-router-dom'
+import SecondFloorMap from './SecondFloorMap';
+import SecondFloorMeetingRoom from './SecondFloorMeetingRoom';
+import SecondFloorNaRoom from './SecondFloorNaRoom';
+import styles from './SecondFloor.module.css';
+import useUrl from '../../../hooks/useUrl';
+import useTimeAlert from '../../../hooks/useTimeAlert';
+import { useState, useEffect } from 'react';
+import { fetchGet } from '../../../hooks/fetchUrl';
+import { useLocation } from 'react-router-dom';
 
-const SecondFloor = () => {
-  const myUrl = useUrl()
-  const [ablebtn, BookingConfirm] = useTimeAlert()
-
-  //2층 API 정보 가져오기
-  const [bookingData, setBookingData] = useState([])
-  const [roomData, setRoomData] = useState([])
-
-  const [SinyangID, setSinYangID] = useState('')
-  const [SinyangName, setSinYangName] = useState('')
-
-  const userClasses = window.localStorage.getItem('class')
-
-  const url = `http://${myUrl}/api/booking/main?floor=2`
-  const location = useLocation()
-  useEffect(() => {
-fetchGet(url,location)
-      .then((data) => {
-        setBookingData(data.BookingData)
-        setRoomData(data.RoomData)
-        setSinYangID(
-          data.RoomData.filter((rooms) => rooms.roomId === 207)[0].roomId
-        )
-        setSinYangName(
-          data.RoomData.filter((rooms) => rooms.roomName === '신양')[0].roomName
-        )
-      })
-  }, [url, myUrl])
-
-  const SecondMeetingRoominfo = roomData.filter(
-    (rooms) => rooms.roomType === 'meeting' && rooms.roomName !== '신양'
-  )
-
-  const SecondNaboxinfo = roomData.filter((rooms) => rooms.roomType === 'nabox')
-
+const SecondFloor = ({
+  ablebtn,
+  BookingConfirm,
+  SecondMeetingRoominfo,
+  SecondNaboxinfo,
+  bookingData,
+  roomData,
+  SinyangID,
+  SinyangName,
+  userClasses,
+}) => {
   return (
     <div className={styles.MainSecondFloor}>
       <div className={styles.SecondFloor}>
@@ -83,7 +59,7 @@ fetchGet(url,location)
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SecondFloor
+export default SecondFloor;
