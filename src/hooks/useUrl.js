a56f09116d8514f8e6464042459ee507251088e2
hyunjeong <cchloe0927@gmail.com>
2022-09-21 10:46:29 +0900

    useUrl api 변경
    변경사항 : useUrl api 변경

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,8 +1,5 @@
 export default function useUrl() {
-
-  const url = process.env.REACT_APP_BACK_URL;
-  // const url = process.env.REACT_APP_BACK_Dev_URL;
-  // const url = "192.168.5.92:8080"
-
+  // const url = process.env.REACT_APP_BACK_URL;
+  const url = process.env.REACT_APP_BACK_Dev_URL;
   return url;
 }
