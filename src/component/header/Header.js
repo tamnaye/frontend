280e7f659c6573f1bfb918c3e2dea743c07f78e8
mugyeol <htpadkorik@naver.com>
2022-09-22 14:45:20 +0900

    edit roomAdmin & admin login logout & logout request

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,20 +1,38 @@
 import styles from './Header.module.css';
 import logo from './img/ci_png.png';
-import { Link } from 'react-router-dom';
-import { getAuth } from '../../hooks/authModule';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, removeToken } from '../../hooks/authModule';
+import { fetchGet } from '../../hooks/fetchUrl';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  function logout() {
+      fetchGet(process.env.REACT_APP_PREFIX + "auth/logout", navigate)
+      .then(
+        (data) => {
+          if (data.message === "success") {
+            console.log("logout data : ", data);
+            removeToken();
+            navigate("/");
+          }else{
+            alert("로그아웃 실패 : 관리자에게 문의하세요")
+          }
+        }
+      );
+  }
   return (
     <div className={styles.HeaderContainer}>
       <div className={styles.Header}>
         <Link to={`/main`}>
           <img className={styles.img} src={logo} alt='logo'></img>
         </Link>
+        {/* 1890FF */}
         <div className={styles.anker}>
           {getAuth().auth === null ? (
             <Link to='/'>Login</Link>
           ) : (
-            <Link to='/logout'>Logout</Link>
+            <span onClick={logout}>Logout</span>
           )}
           <Link to={`/mypage`}>My page</Link>
         </div>
