63fbbae85ef430d56f1fe99e243f65542bec19cb
SooBin An <jejupalette@naver.com>
2022-10-04 09:56:35 +0900

    test

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,6 +1,6 @@
 export default function useUrl() {
-  const url = process.env.REACT_APP_BACK_URL_TEST;
-  // const url = process.env.REACT_APP_BACK_Dev_URL;
+  // const url = process.env.REACT_APP_BACK_URL_TEST;
+  const url = process.env.REACT_APP_BACK_Dev_URL;
 
   return url;
 }
