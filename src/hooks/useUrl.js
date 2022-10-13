3a54a7e98757e63e0ada57e4441098c575c85134
이현정 <cchloe0927@gmail.com>
2022-10-13 14:40:18 +0900

    url

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,8 +1,8 @@
 export default function useUrl() {
 
-  // const url = process.env.REACT_APP_BACK_UR;
+  const url = process.env.REACT_APP_BACK_UR;
   //const url = process.env.REACT_APP_BACK_URL_TEST;
-  const url = process.env.REACT_APP_BACK_URL_TEST2;
+
   
   return url;
 }
